0b7b844a7f3deebec158769d0ace9c4d44995ea0
TejashriChouke <139746868+TejashriChouke@users.noreply.github.com>
2023-09-13 23:04:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
 
+const players = ["John", "Bob", "Alice", "Poppy"];
 const person = {
   name: "John Doe",
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Create a reference to the 'players' array
+const team = players;
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+// Create a shallow copy of the 'players' array
+const team1 = [...players];
+
+// Create a shallow copy of the 'person' object
+const cap1 = { ...person };
+
+// Testing to make sure modifications don't affect the original arrays/objects
+team.push("Megan");
+team1.push("Eva");
+cap1.name = "Jane Smith";
+
+console.log("Original players array:", players);
+console.log("team:", team);
+console.log("team1:", team1);
+console.log("Original person object:", person);
+console.log("cap1:", cap1);
+
